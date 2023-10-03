605e3412129078e75a95a6dbbf8aad6c55d95def
Brad Turner <bradturner43@gmail.com>
2023-10-03 13:19:50 -0500

    Updated to Ashita v4 by Zal Das and GetAwayCoxn!

--- ticker.lua
+++ ticker.lua
@@ -1,210 +1,96 @@
 --[[
-* Ashita - Copyright (c) 2014 - 2022 atom0s [atom0s@live.com]
+* Addons - Copyright (c) 2021 Ashita Development Team
+* Contact: https://www.ashitaxi.com/
+* Contact: https://discord.gg/Ashita
 *
-* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
-* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to
-* Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+* This file is part of Ashita.
 *
-* By using Ashita, you agree to the above license and its terms.
+* Ashita is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 *
-*      Attribution - You must give appropriate credit, provide a link to the license and indicate if changes were
-*                    made. You must do so in any reasonable manner, but not in any way that suggests the licensor
-*                    endorses you or your use.
+* Ashita is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 *
-*   Non-Commercial - You may not use the material (Ashita) for commercial purposes.
-*
-*   No-Derivatives - If you remix, transform, or build upon the material (Ashita), you may not distribute the
-*                    modified material. You are, however, allowed to submit the modified works back to the original
-*                    Ashita project in attempt to have it added to the original project.
-*
-* You may not apply legal terms or technological measures that legally restrict others
-* from doing anything the license permits.
-*
-* No warranties are given.
-]]--
+* You should have received a copy of the GNU General Public License
+* along with Ashita.  If not, see <https://www.gnu.org/licenses/>.
+--]]
 
-_addon.author   = 'Almavivaconte';
-_addon.name     = 'ticker';
-_addon.version  = '0.0.1';
+addon.author   = 'Almavivaconte (ported to Ashita v4 by Zal Das, GetAwayCoxn)';
+addon.name     = 'ticker';
+addon.version  = '1.0';
 
-require 'common'
-
-local colors = {   
-    4279301120,
-    4280084480,
-    4280867840,
-    4281651200,
-    4282434560,
-    4283217920,
-    4284001280,
-    4284784640,
-    4285568000,
-    4286351360,
-    4287134720,
-    4287918080,
-    4288701440,
-    4289484800,
-    4290268160,
-    4291051520,
-    4291834880,
-    4292618240,
-    4293401600,
-    4294184960,
-    4294901760
-}
-
-local ticker_config =
-{
-    font =
-    {
-        family      = 'Arial',
-        size        = 6,
-        color       = 0xFFFFFFFF,
-        position    = { -30, -30 },
-        bgcolor     = 0x80000000,
-        bgvisible   = true
-    },
+require ('common');
+local settings = require('settings');
+local fonts = require('fonts');
+local display = {};
+local osd = {};
+local defaults = T{
+	visible = true,
+	font_family = 'Arial',
+	font_height = 24,
+	color = 0xFFFFFFFF,
+	position_x = 500,
+	position_y = 500,
+	background = T{
+		visible = true,
+		color = 0xFF000000,
+	}
 };
 
-local tickerBlocked = false;
-local entityBlocked = false;
-local tickTime = 21;
-local currentMP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentMP(0);
-local currentHP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentHP(0);
-local currenstatus = 0;
+local tickTime = 20;
+local party = AshitaCore:GetMemoryManager():GetParty();
+local _timer = 0;
 
-local function tickerUnblocker()
-    tickerBlocked = false;
-end
-
-local function tickerBlocker()
-    tickerBlocked = true;
-    ashita.timer.once(.25, tickerUnblocker);
-end
-
-local function unblockEntity()
-    entityBlocked = false;
-end
-
-local function blockEntity()
-    entityBlocked = true;
-    ashita.timer.once(20, unblockEntity);
-end
-
-local currentLevel = AshitaCore:GetDataManager():GetPlayer():GetMainJobLevel();
-local levelMod;
-if(currentLevel <= 25) then
-    levelMod = 0;
-elseif(currentLevel <= 40) then
-    levelMod = 5;
-elseif(currentLevel <= 60) then
-    levelMod = 7;
-else
-    levelMod = 10;
-end
-
-local function getStatus(index)
-    return GetEntity(index).Status;
-end
-
-local function errHandler()
-    return;
-end
+ashita.events.register('load', 'load_cb', function()
+    osd.settings = settings.load(defaults);
     
-
-----------------------------------------------------------------------------------------------------
--- func: load
--- desc: Event called when the addon is being loaded.
-----------------------------------------------------------------------------------------------------
-ashita.register_event('load', function()
-    -- Attempt to load the configuration..
-    ticker_config = ashita.settings.load_merged(_addon.path .. 'settings/settings.json', ticker_config);
-
-    -- Create our font object..
-    local f = AshitaCore:GetFontManager():Create('__ticker_addon');
-    f:SetColor(ticker_config.font.color);
-    f:SetFontFamily(ticker_config.font.family);
-    f:SetFontHeight(ticker_config.font.size);
-    f:SetBold(false);
-    f:SetPositionX(ticker_config.font.position[1]);
-    f:SetPositionY(ticker_config.font.position[2]);
-    f:SetVisibility(false);
-    f:GetBackground():SetColor(ticker_config.font.bgcolor);
-    f:GetBackground():SetVisibility(ticker_config.font.bgvisible);
+    display = fonts.new(osd.settings);
 end);
 
-----------------------------------------------------------------------------------------------------
--- func: unload
--- desc: Event called when the addon is being unloaded.
-----------------------------------------------------------------------------------------------------
-ashita.register_event('unload', function()
-    local f = AshitaCore:GetFontManager():Get('__ticker_addon');
-    ticker_config.font.position = { f:GetPositionX(), f:GetPositionY() };
-    -- Save the configuration..
-    ashita.settings.save(_addon.path .. 'settings/settings.json', ticker_config);
-    
-    -- Unload the font object..
-    AshitaCore:GetFontManager():Delete('__ticker_addon');
-end );
+ashita.events.register('unload', 'unload_cb', function()
+    settings.save();
 
----------------------------------------------------------------------------------------------------
--- func: Render
--- desc: Called when our addon is rendered.
----------------------------------------------------------------------------------------------------
-ashita.register_event('render', function()
-    
-    local f = AshitaCore:GetFontManager():Get('__ticker_addon');
-    local selfIndex;
-    if not entityBlocked then
-        selfIndex = AshitaCore:GetDataManager():GetParty():GetMemberTargetIndex(0);
-        currentStatus = GetEntity(selfIndex).Status;
-    end
-    
-    if tickTime >= 1 and tickTime <= 21 then
-        f:SetColor(colors[tickTime]);
-    end
-    f:SetText(tostring(tickTime));
-    
-    if currentStatus ~= nil then
-        if currentStatus == 33 and not (AshitaCore:GetDataManager():GetParty():GetMemberCurrentMPP(0) == 100 and AshitaCore:GetDataManager():GetParty():GetMemberCurrentHPP(0) == 100) then
-            f:SetVisibility(true);
-            if not tickerBlocked then
-                tickerBlocker();
-                if tickTime > 0 then
-                    tickTime = tickTime - 1;
-                    if tickTime == 4 then
-                    currentMP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentMP(0);
-                    currentHP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentHP(0);
-                    end
-                end 
-                if AshitaCore:GetDataManager():GetParty():GetMemberCurrentMP(0) - currentMP > (10 + levelMod) or AshitaCore:GetDataManager():GetParty():GetMemberCurrentHP(0) - currentHP > (10 + levelMod) then
-                    tickTime = 10;
-                    currentMP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentMP(0);
-                    currentHP = AshitaCore:GetDataManager():GetParty():GetMemberCurrentHP(0);
-                end
-            end
-        else
-            tickTime = 21;
-            f:SetVisibility(false);
-        end
-    end
-    
-    return;
+    if (display ~= nil) then
+		display:destroy();
+	end
 end);
 
----------------------------------------------------------------------------------------------------
--- func: incoming_packet
--- desc: Called when our addon receives an incoming packet.
----------------------------------------------------------------------------------------------------
-ashita.register_event('outgoing_packet', function(id, size, data, modified, blocked)
-    
-    if (id == 0x00D) then
-        local f = AshitaCore:GetFontManager():Get('__ticker_addon');
-        entityBlocked = true;
-        f:SetVisibility(false);
-    elseif (id == 0x0E8) then
-        entityBlocked = false;
+ashita.events.register('d3d_present', 'present_cb', function ()
+	-- only run this once a second
+	if (os.time() >= _timer + 1) then
+		_timer = os.time();
+
+		selfIndex = party:GetMemberTargetIndex(0);
+		if selfIndex ~= nil then
+			local me = GetEntity(selfIndex)
+			if me ~= nil then
+				currentStatus = me.Status;
+			end
+		end
+
+		if currentStatus ~= nil then
+			if currentStatus == 33 then
+				display.text = tostring(tickTime);
+				display.visible = true;
+				if tickTime > 1 then
+					tickTime = tickTime - 1;
+				else
+					tickTime = 10;
+				end 
+			else
+				tickTime = 20;
+				display.visible = false;
+			end
+		end
+	end
+
+    if display.position_x ~= osd.settings.position_x or display.position_y ~= osd.settings.position_y then
+        osd.settings.position_x = display.position_x;
+        osd.settings.position_y = display.position_y;
+        settings.save();
     end
-    return false;
-    
 end);
